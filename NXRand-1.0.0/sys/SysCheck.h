8fc35c8d2f18e51419863ffc4e84056533597982
Arnyxa <arnyxxa@gmail.com>
2018-08-27 13:40:58 +0200

    Updated gitignore and ignored files

--- NXRand-1.0.0/sys/SysCheck.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined __x86_64__ || defined _M_AMD64 || defined __IA64__ || defined _M_IA64 || __ppc64__ || _M_PPC
-#define NX64
-#elif defined _WIN32 || defined _M_IX86 || defined _X86_ || defined __MINGW32_MAJOR_VERSION || defined __MINGW32_MINOR_VERSION || defined __i386__
-#define NX32
-#else
-#define NXUNKNOWN
-#endif
--- include/NXRand.h
+++ include/NXRand.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../sys/incl.h"
+#include "../sys/include.h"
 
 namespace nx
 {
