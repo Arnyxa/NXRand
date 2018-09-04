b0b5f5e5ad2ff7bd86d05df6b08ba0072e6d4cd4
Arnyxa <arnyxxa@gmail.com>
2018-09-04 17:13:44 +0200

    documentation is harder than programming

--- Example.cpp
+++ Example.cpp
@@ -1,27 +1,30 @@
 #include "include/NXRand.h"
 #include <iostream>
 
-int main() 
+int main()
 {
-	nx::Rand<float> myfRand(2.f, 5.f);    // random object that stores floats, in the range 2 to 5
-	nx::Rand<double> mydRand;             // random object that stores doubles, in the range 0.0 to 1.0 (default)
-	nx::Rand<int> myiRand(12, 54);        // random object that stores ints, range 12~54 (integral default range is 1~100)
+	using namespace nx;                   // for nx::Rand
 
-	nx::Rand<unsigned> myUInt(67, 55);    // you can also use other types, as long as they're integral/floating
-	nx::Rand<long double> mySpicy;        // even the spicier types
+	Rand<float> myfRand(2.f, 5.f);        // random object that stores floats, in the range 2 to 5
+	Rand<double> mydRand;                 // random object that stores doubles, in the range 0.0 to 1.0 (default)
+	Rand<int> myiRand(12, 54);            // random object that stores ints, range 12~54 (integral default range is 1~100)
+	 
+	Rand<unsigned> myUInt(67, 55);        // you can also use other types, as long as they're integral/floating
+	Rand<long double> mySpicy;            // even the spicier types
 
-	nx::Rand<bool> myBools(0.7);          // booleans use a range of 0.0 to 1.0 to determine the probability of something being true
-	nx::Rand<bool> myBool;                // the default probability is 0.5, so a roughly equal chance for either
+	Rand<bool> myBools(0.7);              // booleans use a range of 0.0 to 1.0 to determine the probability of something being true
+	Rand<bool> myBool;                    // the default probability is 0.5, so a roughly equal chance for either
 
-	nx::Rand<int>::Next();                // if you don't want to make an object and just want quick rng, you use this
-	nx::Rand<long double>::Next();        // it works with any int/float type
-	nx::Rand<bool>::Next();               // or bool
+	Rand<int>::Next();                    // if you don't want to make an object and just want quick rng, you use this
+	Rand<long double>::Next();            // it works with any int/float type
+	Rand<bool>::Next();                   // or bool
 
-	nx::Rand<int>::Next(18);              // you can specify the max value (min will default to 0)
-	nx::Rand<double>::Next(14.2, 100.85); // or you can specify a min and a max value
+	Rand<int>::Next(18);                  // you can specify the max value (min will default to 0)
+	Rand<double>::Next(14.2, 100.85);     // or you can specify a min and a max value
 
-	nx::Rand<bool>::Next(0.7);            // for booleans, you can specify the probability of the outcome being 'true' instead
-	nx::Rand<bool>::Next();               // do keep in mind this probability stays the same for subsequent calls, you would have to manually set it back to the default 0.5
+	Rand<bool>::Next(0.7);                // for booleans, you can specify the probability of the outcome being 'true' instead
+	Rand<bool>::Next();                   // do keep in mind this probability stays the same for subsequent calls
+                                          // you would have to manually set it back to the default 0.5
 
 	mySpicy.Seed(52);                     // you can specify your own seeds
 	myiRand.ResetRange();                 // resets range to default (1~100)
@@ -29,7 +32,7 @@ int main()
 	mydRand.ResetState();                 // can also reset the internal state of the distribution (it stops caring what number came last)
 	myBools.SetProbability(25.7);         // boolean probabilities are modulated to between 0.0 and 1.0 for any value above 1
 
-	nx::Rand<int> myObject(100, 200);
+	Rand<int> myObject(100, 200);
 
 	for (unsigned i = 0; i < 25; ++i)
 		std::cout << myObject() << "\n";  // you can get a random value within your range like this
@@ -43,10 +46,10 @@ int main()
 		if (e)
 			++count;
 
-		std::cout << e << " ";            
+		std::cout << e << " ";
 	}
 
-	std::cout << "\n" << count << " True\n" 
-		<< total - count << " False";	
+	std::cout << "\n" << count << " True\n"
+		<< total - count << " False";
 
 }
